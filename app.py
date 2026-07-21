5921de1b97a345a430ce731486e8f840aa0bbe7c
dhruvpandit0275-wq <dhruvpandit0275@gmail.com>
2026-07-21 18:56:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def send_premium_mail(target_email, otp, action_name):
                     </tr>
                     <tr>
                         <td class="email-footer">
-                            <p style="margin: 0;">&copy; {datetime.now().year} Nidhi Tech (Apna Print Portal). All rights reserved.</p>
+                            <p style="margin: 0;">&copy; {datetime.now().year}Apna Nidhi Tech. All rights reserved.</p>
                         </td>
                     </tr>
                 </table>
@@ -152,7 +152,7 @@ def send_premium_mail(target_email, otp, action_name):
         """
         
         payload = {
-            "sender": {"email": "contactsapnaportals@11287780.brevosend.com", "name": "Nidhi Tech"},
+            "sender": {"email": "contactsapnaportals@gmail.com", "name": "Nidhi Tech"},
             "to": [{"email": target_email}],
             "subject": f"Security Alert: {action_name} Verification",
             "htmlContent": html_content
